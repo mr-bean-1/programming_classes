c19a6a2bb39d20c6f4f967dc8c5f4dfd913126af
mr-bean-1 <renlau505@byui.edu>
2024-09-28 17:28:52 -0600

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //  guess my number
+
+        Random randomGenerator = new Random();
+        int random_num = randomGenerator.Next(1, 101);
+
+        int guess = 0;
+
+        while (guess != random_num){
+            Console.Write("Choose a random number from 1-100: ");
+
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > random_num) {
+            Console.WriteLine("Guess higher: ");
+            }
+
+            if (guess < random_num) {
+            Console.WriteLine("Guess lower: ");
+            }
+            else {
+                Console.WriteLine("You guessed the right number.");
+            }
+
+            }
+
+        }
+
     }
-}
+
+
+
