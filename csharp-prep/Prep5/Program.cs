262357d829598e2952fde7ca7f82f878fafd1ded
mr-bean-1 <renlau505@byui.edu>
2024-09-28 23:53:11 -0600

    Update Program.cs

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int square = SquareNumber();
+        DisplayResult(square, name);
     }
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName(){
+        Console.WriteLine("What is your name? ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Your name is {name}");
+        return name; 
+
+    }
+
+     static int SquareNumber(){
+        Console.WriteLine("Enter a number: ");
+        string number = Console.ReadLine();
+        int value = int.Parse(number);
+        int square = value * value;  
+        Console.WriteLine($"The squared number is {square}");
+        return square; 
+
+
+
+    }
+
+     static void DisplayResult(int square, string name){
+        Console.WriteLine($"Your name is {name} and the squared num is {square}");
+
+    }
+
+
+
 }
