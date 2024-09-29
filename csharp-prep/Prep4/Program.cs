9725a29dbfb6502c95eebd50aa60d83801360bb3
mr-bean-1 <renlau505@byui.edu>
2024-09-28 23:19:41 -0600

    Update Program.cs

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+      List<int> numbers = new List<int> ();
+
+      int num = -1;  
+      while (num != 0){
+            
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+            string number = Console.ReadLine();
+            num = int.Parse(number);
+
+            if (num != 0){
+                numbers.Add(num);
+            }
+            
+        }
+
+        int sum = 0;
+        foreach (int number in numbers){
+            sum += number;
+        
     }
+    Console.WriteLine($"The ume is: {sum}");
+    float average = ((float)sum) / numbers.Count;
+    Console.WriteLine($"The average is: {average}");
+
+    int max = numbers[0];
+    foreach (int number in numbers){
+        if (number > max){
+            max = number;
+        }
+    }
+    Console.WriteLine($"The max is: {max}");
+    }
+
+
+  
 }
