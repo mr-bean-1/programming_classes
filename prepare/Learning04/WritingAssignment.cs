4e56fd44a7fdeaf2b84587a020b9aa246d9718d7
mr-bean-1 <renlau505@byui.edu>
2024-11-06 00:05:57 -0700

    Create WritingAssignment.cs

---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,23 @@
+using System.Security.Cryptography.X509Certificates;
+
+public class WritingAssignment{
+    private string _title; 
+
+    public WritingAssignment(string studentName, string topic, string title ){
+        : base(studentName, topic){
+
+            _title = title;
+        }
+       public string GetWritingInformation(){
+            string studentName = GetStudentName();
+            return $"{_title} by {studentName}";
+        }
+
+    }
+
+
+
+
+
+    
+}
