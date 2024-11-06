68d6ef3f9bd38c5c0249753abed70ea5c2c4e04f
mr-bean-1 <renlau505@byui.edu>
2024-11-06 00:04:31 -0700

    Update Program.cs

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.WriteLine("Hello Learning03 World!");
+        Assignment a1 = new Assignment("James Bond", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Henry Cavill", "Fraction", "8.2", "6-12");
+        Console.WriteLine(a2.GetSummary); 
+        Console.WriteLine(a2.GetHomeworkList());
+        WritingAssignment a3 = new MathAssignment("Abraham Lincoln", "American History", "The battle of Midway");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
