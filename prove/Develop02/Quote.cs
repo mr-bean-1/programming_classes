74de4f83fa021f075bcbeaf805f96726fb48f1ae
mr-bean-1 <renlau505@byui.edu>
2024-11-13 10:58:12 -0700

    Update Quote.cs

--- prove/Develop02/Quote.cs
+++ prove/Develop02/Quote.cs
@@ -8,7 +8,6 @@ public class Quote
         {
             "It is our job to lift other up, not to size them up. - Neal A. Maxwell",
             "It isn't what we say or think that defines us, but what we do. - Jane Austen",
-            "How did I help someone else today?",
             "Sometimes by losing a battle, you find a new way to win the war. - Donald Trump",
             "He has a right to criticize, who has a heart to help. - Abraham Lincoln",
             "The best way to predict the future is to create it - Abraham Lincoln",
