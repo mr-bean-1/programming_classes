58d90c4c7f872ab56123689cc2df1152e42f35c4
mr-bean-1 <renlau505@byui.edu>
2024-11-21 20:45:22 -0700

    Update Reference.cs

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -16,17 +16,8 @@ public Reference(string book, int chapter, int verse){
     this._endVerse = verse; 
 }
 
-public Reference(string book, int chapter, int startVerse, int endVerse){
-    this._book = book;
-    this._chapter = chapter;
-    this._startVerse = startVerse;
-    this._endVerse = endVerse;
-}
 public override string ToString(){
-    if (_startVerse == _endVerse)
-        return $"{_book} {_chapter}:{_startVerse}";
-    else
-        return $"{_book} {_chapter}:{_startVerse}-{_endVerse}";
+return $"{_book} {_chapter}:{_startVerse}";
 
 
 }
