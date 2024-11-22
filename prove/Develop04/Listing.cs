aa86dcb355570d6cd03fad05941fe08624673e5d
mr-bean-1 <renlau505@byui.edu>
2024-11-21 23:30:04 -0700

    Update Listing.cs

--- prove/Develop04/Listing.cs
+++ prove/Develop04/Listing.cs
@@ -1,11 +1,9 @@
-using System.Data;
+using System; 
+using System.Collections.Generic; 
 
-class Listing{
+class Listing : Activity {
 
-    public void listing_activity(){
-
-        // listing
-        List<string> list_prompts = new List<string>{
+      List<string> listPrompts = new List<string>{
             "Who are people that you appreciate?", 
             "What are personal strenghts of yours?",
             "Who are people that you have helped this week?",
@@ -13,61 +11,46 @@ class Listing{
             "Who are some of your personal heroes?",
         };
 
-        Console.WriteLine("Welcome to the Listing Activity\n");
-        Console.WriteLine("This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.\n");
-        Console.WriteLine("How long, in seconds, would you like this activity to run? ");
-        
-        string list_time_input = Console.ReadLine();
-        int list_time; 
-        // Console.WriteLine("Get ready...");
 
-         if (!int.TryParse(list_time_input, out list_time) || list_time <= 0)
-        {
-            Console.WriteLine("Invalid input. Please enter a valid number.");
-            return;
-        }
-        
-        Console.WriteLine("List as many responses as you can to the following prompt: ");
-        DisplayPrompt(list_prompts);
-        CollectResponses(list_time);
-
-    
-    }
-
-    public void DisplayPrompt(List<string> prompts){
-        Random random = new Random();
-        int randomIndex = random.Next(prompts.Count);
-        Console.WriteLine(prompts[randomIndex]);
-
-    }
-    
-   public void CollectResponses(int seconds)
+   protected override void StartActivity(int time)
     {
-        DateTime endTime = DateTime.Now.AddSeconds(seconds); // Calculate end time
+        Console.WriteLine("Welcome to the Listing Activity.");
+        Console.WriteLine("This activity will help you reflect on the good things in your life.");
+
+        Console.WriteLine("List as many responses as you can to the following prompt: ");
+        DisplayPrompt();
+
+        CollectResponses(time);
+    }
+    
+    public void DisplayPrompt(){
+        Random random = new Random();
+        int randomIndex = random.Next(listPrompts.Count);
+        Console.WriteLine(listPrompts[randomIndex]);
+
+    }
+
+   private void CollectResponses(int seconds)
+    {
+        DateTime endTime = DateTime.Now.AddSeconds(seconds);
         List<string> responses = new List<string>();
 
-        // Start the activity
         while (DateTime.Now < endTime)
         {
-            Console.Write("Enter a response(s): ");
+            Console.Write("Enter a response: ");
             string response = Console.ReadLine();
 
-            // If the user presses Enter without entering anything, we stop early
-            if (string.IsNullOrEmpty(response))
-            {
-                break;
+            if (string.IsNullOrEmpty(response)) {
+                Console.WriteLine("No input detected. ");
+                break; 
             }
 
             responses.Add(response);
             Console.WriteLine("Response recorded.");
         }
 
-        // Show how many responses were collected
-        Console.WriteLine($"\nYou listed {responses.Count} items.\n");
-        Console.WriteLine("Well done!!");
-        Console.WriteLine($"You have completed another {seconds} seconds of the Listing Activity!");
-        Console.WriteLine("Here are your responses:");
-        
+        Console.WriteLine($"You listed {responses.Count} items.");
+        Console.WriteLine("Well done!! Here are your responses:");
         foreach (var response in responses)
         {
             Console.WriteLine($"- {response}");
@@ -106,6 +89,4 @@ class Listing{
 
 
 
-
-
-// }
+// // }
