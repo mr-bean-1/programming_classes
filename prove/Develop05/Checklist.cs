14f6707b1577ca119a3603af3d3bac41d859db18
mr-bean-1 <renlau505@byui.edu>
2024-11-22 20:44:29 -0700

    Create Checklist.cs

---
+++ prove/Develop05/Checklist.cs
@@ -0,0 +1,24 @@
+using System; 
+class Checklist : Goal{
+
+
+   static void checklister(){
+        
+        Console.WriteLine("What is the name of your goal?");
+        Console.WriteLine("What is a short description of your goal?");
+        Console.WriteLine("What is the amount of points associated with this goal?");
+        Console.WriteLine("How many times does this goal need to be completed to get a bonus? ");
+        Console.WriteLine("What is the point bonus for finishing it that many times?");
+        Console.WriteLine("You have _ points");
+
+
+
+    }
+
+
+
+
+
+
+
+}
