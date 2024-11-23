50b9eecc733a15cf5a145c5c2c840fe1b041fa5d
mr-bean-1 <renlau505@byui.edu>
2024-11-22 20:43:59 -0700

    Create Simple.cs

---
+++ prove/Develop05/Simple.cs
@@ -0,0 +1,46 @@
+using System; 
+
+public class Simple : Goal{
+
+    //  ask user 
+    static void simpler(){
+        
+        Console.WriteLine("What is the name of your goal?");
+        Console.WriteLine("What is a short description of your goal?");
+        Console.WriteLine("What is the amount of points associated with this goal?");
+        Console.WriteLine("You have _ points");
+
+
+
+    }
+  
+
+
+    // Goal
+    // GetGoalName
+    // SetGoalName
+    // GetGoalDescription
+    // SetGoalDescription
+    // DispalyGoalPoints
+    // GetGoalPoints
+    // GetGoalStatus
+    // ToFile
+    // RecordEvent
+    // DisplayGoalName
+    // DisplayGoalDescription
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
