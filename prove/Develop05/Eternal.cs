715db6df441579d89895abdc014cb4a03747ebf3
mr-bean-1 <renlau505@byui.edu>
2024-11-22 20:44:14 -0700

    Create Eternal.cs

---
+++ prove/Develop05/Eternal.cs
@@ -0,0 +1,24 @@
+using System; 
+
+class Eternal : Goal{
+
+
+   static void eternaler(){
+        
+        Console.WriteLine("What is the name of your goal?");
+        Console.WriteLine("What is a short description of your goal?");
+        Console.WriteLine("What is the amount of points associated with this goal?");
+        Console.WriteLine("You have _ points");
+
+
+
+    }
+
+
+    
+}
+
+
+
+
+
